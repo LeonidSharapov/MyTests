f7b34d8b2b77ae481cd1753b54b016094c94b1e5
LeonidSharapov <78010280+LeonidSharapov@users.noreply.github.com>
2021-01-26 22:48:33 +0900

    heh

--- src/valera/Main.java
+++ src/valera/Main.java
@@ -1,18 +1,12 @@
 package valera;
 
 import valera.ArraysAndString.Test3;
-
-
 import java.io.*;
-
-
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         System.out.println(Test3.test(reader.readLine()));
     }
 
