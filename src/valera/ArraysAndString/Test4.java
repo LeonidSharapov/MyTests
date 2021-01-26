844f027754a257b8577601e573195a06cc6fb03f
inoy <tbmcze@gmail.com>
2021-01-26 23:26:40 +0900

    test3, test4, main update

--- src/valera/ArraysAndString/Test4.java
+++ src/valera/ArraysAndString/Test4.java
@@ -1,7 +1,7 @@
 package valera.ArraysAndString;
 
 public class Test4 {
-    static boolean test(String a, String b) {
+    public static boolean test(String a, String b) {
         String[] chek2;
         String[] chek1;
         int value = 0;
@@ -10,7 +10,7 @@ public class Test4 {
             chek2 = b.split("");
 
             for (int i = 0; i < chek1.length; i++) {
-                for (int j = 0; j < chek2.length; j++) {
+                for (int j = 0; j < chek2.length; j++) { //вложенный цикл увеличивает время работы в chek2.length раз
                     if (chek2[j].equals(chek1[i])) {
                         chek1[i] = "";
                         chek2[j] = "";
@@ -28,5 +28,31 @@ public class Test4 {
         } else {
             return false;
         }
+    }
+
+    public static boolean test2(String first, String second) {
+        if (Math.abs(first.length() - second.length()) > 1) {
+            return false;
+        }
+        String s1 = first.length() < second.length() ? first : second;
+        String s2 = first.length() < second.length() ? second : first;
+
+        int index1 = 0;
+        int index2 = 0;
+
+        boolean foundDifference = false;
+        while (index2 < s2.length() && index1 < s1.length()) {
+            if (s1.charAt(index1) != s2.charAt(index2)) {
+                if (foundDifference) return false;
+                foundDifference = true;
+                if (s1.length() == s2.length()) {
+                    index1++;
+                }
+            } else {
+                index1++;
+            }
+            index2++;
+        }
+        return true;
     }
 }
--- src/valera/Main.java
+++ src/valera/Main.java
@@ -1,13 +1,36 @@
 package valera;
 
 import valera.ArraysAndString.Test3;
+import valera.ArraysAndString.Test4;
+
 import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(Test3.test(reader.readLine()));
+        System.out.println(Test3.test("Mr John Smith    "));
+        System.out.println(Test3.test2("Mr John Smith    ", 13));
+        String str1 ="pale";
+        String str2 ="ple";
+        System.out.println(Test4.test(str1,str2));
+        System.out.println(Test4.test2(str1,str2));
+        str1 ="pales";
+        str2 ="pale";
+        System.out.println(Test4.test(str1,str2));
+        System.out.println(Test4.test2(str1,str2));
+        str1 ="pales";
+        str2 ="bale";
+        System.out.println(Test4.test(str1,str2));
+        System.out.println(Test4.test2(str1,str2));
+        str1 ="pale";
+        str2 ="bae";
+        System.out.println(Test4.test(str1,str2));
+        System.out.println(Test4.test2(str1,str2));
+        str1 ="paes";
+        str2 ="pales";
+        System.out.println(Test4.test(str1,str2));
+        System.out.println(Test4.test2(str1,str2));
     }
 
     }
