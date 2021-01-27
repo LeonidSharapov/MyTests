6a23cd87fb65fe700f762cc837041631472697a2
LeonidSharapov <78010280+LeonidSharapov@users.noreply.github.com>
2021-01-27 09:20:39 +0900

    heh

---
+++ src/valera/ArraysAndString/Test5.java
@@ -0,0 +1,21 @@
+package valera.ArraysAndString;
+
+public class Test5 {
+   public static String test(String a){
+        String[] b = a.split("");
+        String t=b[0];
+        System.out.println(t);
+        int count=1;
+        for(int i =1; i<b.length;i++){
+            if(b[i].equals(b[i-1])){
+                count++;
+            }
+            else{
+                t+=count+b[i];
+                count=1;
+            }
+        }
+        t+=count;
+        return a.length()<=t.length() ? a:t;
+    }
+}
