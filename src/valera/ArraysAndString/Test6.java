2a45e24a712a5f34bcf4c3753adff1c2eeade196
LeonidSharapov <78010280+LeonidSharapov@users.noreply.github.com>
2021-01-27 14:04:04 +0900

    Create Test6.java

---
+++ src/valera/ArraysAndString/Test6.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test5 {
+    static int[][]  test(int [][] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 0) {
+                    Arrays.fill(arr[i], 0);
+                    list.add(j);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(list.size());
+        int index = 0;
+        for(int i=0;i<list.size();i++){
+
+            for(int j=0; j<arr.length;j++){
+                arr[j][list.get(index)]=0;
+            }
+            index++;
+        }
+        return arr;
+    }
+}
